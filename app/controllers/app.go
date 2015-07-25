68cf8b6e51ece51b61d1e449ea1330e53ae2922c
Amuro Nishizawa <aburos.d@gmail.com>
2015-07-25 22:59:46 +0900

    first commit.

---
+++ app/controllers/app.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"github.com/revel/revel"
+	"github.com/stianeikeland/go-rpio"
+	"fmt"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index(pinValue bool) revel.Result {
+	fmt.Println(pinValue)
+	//pinValue = true
+	return c.Render(pinValue)
+}
+
+func (c App) Gpio13(pinValue bool) revel.Result {
+
+	fmt.Println("GPIO13 pin value is: ", pinValue)
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+	} else {
+		defer rpio.Close() // 関数がリターンする直前に実行されるもの
+		pin := rpio.Pin(17) // Physical pin# 11
+		pin.Output() // Output mode
+		//pin.Mode(rpio.Output)
+
+		if pinValue {
+			pin.High() // Set pin High
+			//pin.Write(rpio.High)
+		} else {
+			pin.Low() // Set pin Low
+			//pin.Write(rpio.Low)
+		}	
+		//pin.Toggle() // Toggle pin (Low -> High -> Low)
+	}
+	return c.Redirect(App.Index)
+}
